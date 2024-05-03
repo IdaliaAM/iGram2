ce8fd0e63430a1eb781e5c198fc8ec9631a3a317
IdaliaAM <4bo.idalia.alavez@gmail.com>
2024-05-03 09:11:52 -0600

    Error HomeController2

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LikeController;
+use App\Http\Controllers\ComentarioController;
+use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PerfilController;
-use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\ComentarioController;
-use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,20 +24,22 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', HomeController::class)->name('home');
+//Route::get('/', HomeController::class)->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-//Rutas para el perfil
-Route::get('/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
-Route::post('/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
 
 Route::get('/cuenta', [RegisterController::class, 'index'])->name('register');
 Route::post('/cuenta', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
+
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
-Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
+// Rutas para perfil
+Route::get('/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
+Route::post('/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
+
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
@@ -46,10 +49,12 @@ Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'stor
 
 Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
 
-//Like a las fotos
+// Like fotos
 Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
 Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
 
-//Siguiendo a Usuarios
-Route::post('/{user:username}/follow', [FollowerController::class, 'store'])->name('users.follow');
-Route::delete('/{user:username}/unfollow', [FollowerController::class, 'destroy'])->name('users.unfollow');
+Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
+
+// Siguiendo a Usuarios
+Route::post('/{user:username}/follow', [FollowerController::class,'store'])->name('users.follow');
+Route::delete('/{user:username}/unfollow', [FollowerController::class,'destroy'])->name('users.unfollow');
